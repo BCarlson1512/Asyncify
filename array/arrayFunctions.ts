1525e38b07db42a89c6ebd6c2ec6f0cc231ad487
Ben <46061491+BCarlson1512@users.noreply.github.com>
2022-05-22 00:51:09 -0400

    Refactored, added arrays readme.md

--- array/arrayFunctions.ts
+++ array/arrayFunctions.ts
@@ -1,10 +1,21 @@
-module.exports = {
-    asyncFind: async function asyncFind(arr:any[], predicate) {
-        const result = await Promise.all(arr.map(predicate));
-        return arr.find((_v, index) => result[index]);
-    },
-    asyncFilter: async function asyncFilter(arr:any[], predicate) { // array.find and array.filter are not meant for async use
-        const results = await Promise.all(arr.map(predicate));
-        return arr.filter((_v, index) => results[index]);
-    }
-}
+// async Wrapper for array functions, requires a callback
+export const asyncFind = async (arr:any[], predicate) => {
+    const result = await Promise.all(arr.map(predicate));
+    return arr.find((_v, index) => result[index]);
+};
+
+export const asyncFindIndex = async (arr:any[], predicate) => {
+    const results = await Promise.all(arr.map(predicate));
+    return arr.findIndex((_v, index) => results[index]);
+};
+
+export const asyncSome = async (arr:any[], predicate) => {
+    const results = await Promise.all(arr.map(predicate));
+    if (results.length === 0) return -1;
+    return arr.findIndex((_v, index) => results[index]);
+};
+
+export const asyncFilter = async (arr:any[], predicate) => {
+    const results = await Promise.all(arr.map(predicate));
+    return arr.filter((_v, index) => results[index]);
+};
